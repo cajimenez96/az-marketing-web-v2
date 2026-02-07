a613381adccdd228a526a6ea669ab7566c8f0ad9
Carlos Jimenez <carlos.exejimenez@gmail.com>
2026-02-07 11:18:16 -0300

    refactor: Relocate Home component to pages directory and apply styling and layout enhancements to Hero, AboutUs, and Services components.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { siteConfig } from "@/config/site";
-import Home from "@/components/home/home";
+import Home from "@/pages/Home/home";
+
 // import Particles from "@/components/ui/Particles";
 
 export const metadata: Metadata = {
@@ -13,20 +14,6 @@ export const metadata: Metadata = {
 export default function HomePage() {
   return (
     <>
-      {/* <div className="fixed inset-0 z-10 pointer-events-none">
-        <Particles
-          particleCount={150}
-          particleSpread={12}
-          speed={0.08}
-          particleColors={["#8FB3E2", "#D9E1F1", "#31487A"]}
-          alphaParticles={true}
-          particleBaseSize={80}
-          sizeRandomness={0.8}
-          cameraDistance={25}
-          moveParticlesOnHover={true}
-          particleHoverFactor={0.5}
-        />
-      </div> */}
       <Home />
     </>
   );
--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -7,30 +7,35 @@ interface IHero {
 
 const Hero = ({ id }: IHero) => {
   return (
-    <section id={id} className="h-screen mx-auto pb-10">
+    <section
+      id={id}
+      className="h-screen mx-auto pt-[15%] md:pt-[10%] lg:pt-[5%]"
+    >
       <div className="h-full flex flex-col justify-evenly items-center gap-10">
         <div className="w-fit">
           <div className="flex flex-col justify-center items-center">
             <Typography
               variant="display 1"
-              className="text-[3rem] lg:text-[8rem] xl:text-[9.37rem]"
+              className="text-[50px] md:text-[100px] lg:text-[150px]"
             >
               NO SOMOS SOLO
             </Typography>
-            <div className="bg-tonico w-fit px-5 rounded-[6px] -rotate-1 -mt-3 lg:-mt-12">
+            <div className="bg-tonico w-fit px-5 rounded-[4px]! -rotate-1 -mt-4 md:-mt-8 lg:-mt-12">
               <Typography
                 variant="display subtitle 1"
-                className="text-dominante text-[1.32rem] lg:text-[3.5rem] xl:text-[4.6rem]"
-                style={{ letterSpacing: "0.1em" }}
+                className="text-dominante text-[23px] md:text-[50px] lg:text-[78px]"
+                style={{ letterSpacing: ".15rem" }}
               >
                 UNA AGENCIA DE MARKETING
               </Typography>
             </div>
           </div>
-          <div className="text-center max-w-sm lg:max-w-md mt-10 mx-auto">
-            <Typography variant="subtitle 1" className="text-md lg:text-lg">
-              Te ayudamos a crecer integrando contenido para redes, desarrollo
-              web y publicidad en vía publica .
+          <div className="text-center mt-[52px] mx-auto">
+            <Typography
+              variant="subtitle 1"
+              className="text-[18px]! md:text-[24px]! whitespace-break-spaces height leading-responsive"
+            >
+              {`Te ayudamos a crecer integrando\n contenido para redes, desarrollo web\n y publicidad en vía publica.`}
             </Typography>
           </div>
         </div>
